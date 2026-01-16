50488353c738fa8f6487c845f3f15cb1a9488156
tsykunov <ntmrkx@gmail.com>
2026-01-16 11:03:25 +0100

    pom xml + world json

--- .idea/src/game/model/Room.java
+++ .idea/src/game/model/Room.java
@@ -1,4 +1,27 @@
 package game.model;
 
+import java.util.HashMap;
+
 public class Room {
+    private String name;
+    private String description;
+    private HashMap<String, String> exits;
+
+    public Room(String name, String description, HashMap<String, String> exits) {
+        this.name = name;
+        this.description = description;
+        this.exits = exits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public HashMap<String, String> getExits() {
+        return exits;
+    }
 }
