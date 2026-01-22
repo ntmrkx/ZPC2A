2f045054481c5c08c1687dbe9fb110b0f0c9a0e1
dirolnahh <ntmrkx@gmail.com>
2026-01-22 21:10:12 +0100

    import gson, json, location upgrade

--- .idea/src/game/GameData.java
+++ .idea/src/game/GameData.java
@@ -1,7 +1,7 @@
 package game;
 
 import com.google.gson.Gson;
-import game.model.Location;
+import game.Location;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
