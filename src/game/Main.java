8259f73ebd7a3c2c66ce6aa02b1df18ccd65af47
dirolnahh <ntmrkx@gmail.com>
2026-01-30 11:33:11 +0100

    simplified

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,8 +2,6 @@ package game;
 
 public class Main {
     public static void main(String[] args) {
-        World world = World.loadGameDataFromResources("/world.json");
-        Game game = new Game(world);
-        game.run();
+        new Game().run();
     }
 }
